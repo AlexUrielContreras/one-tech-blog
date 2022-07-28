24bd9ed7d0130e418fbc0718735363b0f9b4d419
Alex Contreras <alex.code29@gmail.com>
2022-07-28 02:45:37 -0500

    feat: initialize express router and bulit api route to create user

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const routes = require('./controllers');
+
 // express middleware
 // Parses incoming request with json payload 
 app.use(express.json());
 // Parses incoming request with urlencoded payloads 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
