5923bb3401f3ff62a4bd8764ca928cfbdfb7de0a
Alex Contreras <alex.code29@gmail.com>
2022-07-27 23:08:58 -0500

    feat: built the user model with validators

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class User extends Model{};
+
+User.init({
+    
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    }, 
+
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: {
+                args: [4, 20],
+                meg: 'Password must be between 4 and 20 characters long'
+            }
+        }
+    }
+},
+{
+    sequelize,
+    // disables the creation of createdAt and updatedAt
+    timestamps: false,
+    // stops the auto pluralization of model name by sequelize
+    freezeTableName: true,
+    underscored: true,
+    tableName: 'user'
+})
