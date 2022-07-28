64d2fb9b600cc7e7ace6e5a1bb7a3b79de3667cc
Alex Contreras <alex.code29@gmail.com>
2022-07-27 22:36:44 -0500

    feat: initialize a connection to the database and setup express server and middleware to parse incoming request

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS one_tech_blog;
+
+CREATE DATABASE one_tech_blog;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const sequelize = require('./config/connections')
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// express middleware
+// Parses incoming request with json payload 
+app.use(express.json());
+// Parses incoming request with urlencoded payloads 
+app.use(express.urlencoded({ extended: true }))
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Now listening on port: ${PORT}`)
+    })
+})
