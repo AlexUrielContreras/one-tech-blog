28c5521d7df1ecb539f541fb72c36c01925176d9
Alex Contreras <alex.code29@gmail.com>
2022-07-29 04:21:05 -0500

    chore: install express-handlebars and set up the view engine

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const sequelize = require('./config/connections')
 const app = express();
+const { engine } = require('express-handlebars');
 
 const PORT = process.env.PORT || 3001;
 
 const routes = require('./controllers');
 
-// express middleware
+// EXPRESS MIDDLEWARE
 // Parses incoming request with json payload 
 app.use(express.json());
 // Parses incoming request with urlencoded payloads 
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-app.use(routes)
+// EXPRESS HANDLEBARS 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
