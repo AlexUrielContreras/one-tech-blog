816c9226912de11b8f0dcb500ce5d632fea82c0f
Alex Contreras <alex.code29@gmail.com>
2022-07-31 03:34:58 -0500

    feat: users can now login with existing accounts

---
+++ public/javascript/login.js
@@ -0,0 +1,40 @@
+async function loginFormHandler(e) {
+    e.preventDefault();
+
+    const email = document.querySelector('#login-email').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email, 
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/')
+        } else {
+            alert(response.statusText)
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler)
