3f3855a961a8e52498d73bc636d1dce569878d88
Alex Contreras <alex.code29@gmail.com>
2022-08-16 19:36:21 -0500

    bug: add route for single page post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,6 +36,38 @@ router.get('/login', (req, res) => {
     res.render('signup')
 })
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'createdAt'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
 
+        if (!dbPostData) {
+            res.status(404).end();
+            return;
+        }
+        
+        const post = dbPostData.get({plain: true});
+        res.render('single-post', {post, loggedIn: req.session.loggedIn})
+    
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
 
 module.exports = router
